7ddf54968fbcbc9ddb9553c945318f7b9020c766
Chuasyeng Vue <152819023+ChuasyengVue@users.noreply.github.com>
2024-03-08 12:34:03 -0500

    Add files via upload

---
+++ Web works/app.js
@@ -0,0 +1,41 @@
+// Part One: Solidify Terminology
+
+// What is HTTP?
+// HTTP (Hypertext transfer protocol) load pages on the internet using hyperlinks.
+// It is designed to transfer information between network devices.
+
+// What is URL?
+// URL (Uniform resource locator) is an mechanism used to retrieve any resource on the web.
+
+// What is DNS?
+// DNS (Domain name system) it is reference as a phonebook.
+// It is used to get information online through ip addresses.
+
+// What is query string?
+// Query string are characters that is part of the URL search.
+// It begins after the question mark with one or more parameters.
+// Ex: www.example.com/search.aspx?q=example
+
+// What are two HTTP verbs and how are they different?
+// Two of HTTP verbs are GET & PUT
+// GET allow users to retreive data on a server, while PUT allow users to update or replace resources.
+
+// What is an HTTP request?
+// HTTP request is referred to HTTP verbs. It is access a resource on servers.
+
+// What is an HTTP response?
+// HTTP response is made from the server to the client. It informs the client if the resource it requested has been carried out or if there's been an error.
+
+// What is an HTTP header?
+// HTTP header allows the server to pass additional information with HTTP request and response, followed after a semicolon : .
+// Ex: :host:facebook.com | :method: Post
+
+// What are the processes that happen when you type "http:somesite.com/some/page.html" into a browser?
+// 1. The browser looks up the IP address using DNS.
+// 2. The browser makes a request to the IP address
+// 3. The server sends a response
+// 4. The browser makes the DOM from HTML
+// 5. The browser makes HTTP requests and receives response from each server.
+
+
+
